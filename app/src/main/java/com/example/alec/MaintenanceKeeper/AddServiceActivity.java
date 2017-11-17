dfdf9e8bc03d083fd70bdc1256fdb1605e8c69d3
Alec Sawatzky <alecsawatzky@gmail.com>
2017-11-17 11:38:50 -0600

    Added Add Service functionality.

--- app/src/main/java/com/example/alec/MaintenanceKeeper/AddServiceActivity.java
+++ app/src/main/java/com/example/alec/MaintenanceKeeper/AddServiceActivity.java
@@ -1,24 +1,25 @@
 package com.example.alec.MaintenanceKeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import static android.R.attr.key;
-
 public class AddServiceActivity extends AppCompatActivity
 {
     private FirebaseDatabase database;
     private DatabaseReference dbReference;
-    private Button btnAddVehicle;
-    private EditText etMake;
-    private EditText etModel;
-    private EditText etYear;
+    private Button btnAddService;
+    private EditText etService;
+    private EditText etDate;
+    private EditText etNotes;
+    private String vehicleKey;
 
 
     @Override
@@ -28,20 +29,40 @@ public class AddServiceActivity extends AppCompatActivity
         setContentView(R.layout.activity_add_service);
         setupActionBar();
 
-        String vehicleKey = getIntent().getStringExtra("vehicleKey");
+        etService = (EditText) findViewById(R.id.etService);
+        etDate = (EditText) findViewById(R.id.etDate);
+        etNotes = (EditText) findViewById(R.id.etNotes);
+        btnAddService = (Button) findViewById(R.id.btnAddService);
+
+        vehicleKey = getIntent().getStringExtra("vehicleKey");
         database = FirebaseDatabase.getInstance();
         dbReference= database.getReference("Vehicles/" + vehicleKey + "/services");
 
         //database.getReference().child("Vehicles").orderByChild("make").equalTo("GMC");
 
+//        // Write to the database.
+        btnAddService.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Service service = new Service(etService.getText().toString(), etDate.getText().toString());
+                dbReference.push().setValue(service);
 
+                Intent intent = new Intent(AddServiceActivity.this, ShowVehicleActivity.class);
+                intent.putExtra("id", vehicleKey);
+                startActivity(intent);
+                finish();
+            }
+        });
 
-
-        Service service = new Service("engine tune up", "10-10-2017");
-        dbReference.push().setValue(service);
-
-
-
+//        Service service = new Service("engine tune up", "10-10-2017");
+//        dbReference.push().setValue(service);
+//
+//        Intent intent = new Intent(AddServiceActivity.this, ShowVehicleActivity.class);
+//        intent.putExtra("id", vehicleKey);
+//        startActivity(intent);
+//        finish();
     }
 
     private void setupActionBar()
