d9308f5979bfea10e82eefb9012bbdf6d35aa6f3
Alec Sawatzky <alecsawatzky@gmail.com>
2017-11-28 19:59:43 -0600

    Added Calandar View and validations on Textboxes for new Vehicle and Service entries.

--- app/src/main/java/com/example/alec/MaintenanceKeeper/AddServiceActivity.java
+++ app/src/main/java/com/example/alec/MaintenanceKeeper/AddServiceActivity.java
@@ -1,12 +1,19 @@
 package com.example.alec.MaintenanceKeeper;
 
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.icu.util.TimeZone;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,10 +24,12 @@ public class AddServiceActivity extends AppCompatActivity
     private DatabaseReference dbReference;
     private Button btnAddService;
     private EditText etService;
-    private EditText etDate;
     private String vehicleKey;
+    private String dateChosen;
+    private CalendarView calendarView;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -29,12 +38,24 @@ public class AddServiceActivity extends AppCompatActivity
         setupActionBar();
 
         etService = (EditText) findViewById(R.id.etService);
-        etDate = (EditText) findViewById(R.id.etDate);
         btnAddService = (Button) findViewById(R.id.btnAddService);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
 
         vehicleKey = getIntent().getStringExtra("vehicleKey");
         database = FirebaseDatabase.getInstance();
-        dbReference= database.getReference("Vehicles/" + vehicleKey + "/services");
+        dbReference = database.getReference("Vehicles/" + vehicleKey + "/services");
+
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+
+        dateChosen = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)) + "-" + String.valueOf(calendar.get(Calendar.MONTH) + 1) + "-" + String.valueOf(calendar.get(Calendar.YEAR));
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                dateChosen = String.valueOf(i2) + "-" + String.valueOf(i1 + 1) + "-" + String.valueOf(i);
+                Toast.makeText(AddServiceActivity.this, dateChosen, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // Write to the database.
         btnAddService.setOnClickListener(new View.OnClickListener()
@@ -42,13 +63,20 @@ public class AddServiceActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                Service service = new Service(etService.getText().toString(), etDate.getText().toString());
-                dbReference.push().setValue(service);
+               if (!etService.getText().toString().trim().equals(""))
+               {
+                   Service service = new Service(etService.getText().toString(), dateChosen);
+                   dbReference.push().setValue(service);
 
-                Intent intent = new Intent(AddServiceActivity.this, ShowVehicleActivity.class);
-                intent.putExtra("id", vehicleKey);
-                startActivity(intent);
-                finish();
+                   Intent intent = new Intent(AddServiceActivity.this, ShowVehicleActivity.class);
+                   intent.putExtra("id", vehicleKey);
+                   startActivity(intent);
+                   finish();
+               }
+               else 
+               {
+                   Toast.makeText(AddServiceActivity.this, "Please enter the Service.", Toast.LENGTH_SHORT).show();
+               }
             }
         });
     }
